e5f64f4294ac877cb564c52324a7945fbee47e4b
“Parima <parima.fen@gmail.com>
2022-02-18 14:14:22 +0700

    intro to js v.2

--- intro-to-js/fen.js
+++ intro-to-js/fen.js
@@ -1,12 +1,10 @@
-// function getmyname(){
-//     alert('Plaese')
-//   }
+
 
 
 function askMyname(){
-  const name = prompt('Please enter your name', "Parima");
+  const name = prompt('Please enter your name', '');
   if (name !=null){
-    document.getElementById('myname').innerHTML = "My name is " + name;
+    document.getElementById('myname').innerHTML = "Hi! " + name;
   }
 }
 
@@ -14,4 +12,18 @@ function helloWorld(){
   const h1message = document.getElementById('myname').innerHTML
   
   alert(h1message + ' ซุปเปอร์วาเลนไทน์')
+}
+
+function calAge(){
+  const age = prompt("What is you birth year", "");
+  const d = new Date();
+  let year = d.getFullYear();
+  alert("ยินดีด้วย นายอยู่ในประเทศนี้มา " + (year - age) + " ปีแล้ว") 
+}
+
+function loadPeofile(){
+  myName = "Fen" ;
+  myProvince = "Canada" ;
+  document.getElementById("my-name").innerHTML = myName ;
+  document.getElementById("my-province").innerHTML = myProvince ;
 }
